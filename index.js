cf3730be972cf3b127bf4ad4191f3b7cf9444a5f
Kanhaiya singhal <57009885+2019uec1035@users.noreply.github.com>
2022-01-04 19:45:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import cors from 'cors'
+import path from "path"
+import { fileURLToPath } from 'url';
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express()
+
+var corsOption = {
+    origin: "*"
+};
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/temp.html");
+});
+
+const PORT = 3000
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(__dirname)
+    console.log(`Server is running on port ${PORT}.`);
+});
